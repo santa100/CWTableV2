50ec346acd5c8af00fb9b34832931e0bdd94fac4
santa100 <114744452+santa100@users.noreply.github.com>
2022-10-03 12:00:32 +0200

    Update and rename newtableB8.js to newtableB9.js

--- newtableB9.js
+++ newtableB9.js
@@ -78,7 +78,7 @@
   // vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
   // HTML extension with all necessary logic(s) wrtitten JS vvvvvvvvvvvv
   // vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv  
-  class myNewTableB8 extends HTMLElement {
+  class myNewTableB9 extends HTMLElement {
     constructor () {
       super()
 
@@ -167,7 +167,7 @@
               
               console.log(" Measure: " + description + "  =   " + formattedValue)
             
-              const { id, description } = dp['/ROH/OPSSTDAT']
+              var { id } = dp['/ROH/OPSSTDAT']
               console.log(a)
               console.log(b)            
               
@@ -231,6 +231,6 @@
   // vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
   // Return the end result to SAC (SAP ANALYTICS CLOUD) application vvvvvvvvvvvvvvvvvvvvv
   // vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
-  customElements.define('com-sap-sample-newtableb8', myNewTableB8)
+  customElements.define('com-sap-sample-newtableb9', myNewTableB9)
   
 })() // END of function --> (function () {
